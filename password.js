ad2d23e1e6f5e648fd402e77d6d06ec3dcf14e72
Jared Shakespaere <jaredshakespaere@Jareds-MacBook-Pro.local>
2021-09-30 20:39:10 -0500

    redo-password-validation

--- password.js
+++ password.js
@@ -1,23 +1,41 @@
 
 
-function greeting(name){
-    console.log(`Hello, ${name}! What is your password?`)
-}
-greeting('')
+// function greeting(name){
+//     console.log(`Hello, ${name}! What is your password?`)
+// }
+// greeting('')
 
-////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////////////
 
-function passwordValidator(attemptedPassword){
-    if(attemptedPassword.length >= 10){
-        console.log('Success!')
-    } else {
-        console.log('Failure to validate')
-    }
+// function passwordValidator(attemptedPassword){
+//     if(attemptedPassword.length >= 10){
+//         console.log('Success!')
+//     } else {
+//         console.log('Failure to validate')
+//     }
     
-}
-passwordValidator('')
+// }
+// passwordValidator('')
 
 
 
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+   
+  readline.question('Hello, What is your name?', name => {
+    console.log(`Hey there ${name}!`);
+    // readline.close();
+  
+  readline.question(`What is your password?`, password => {
+        if(password.length >= 10){
+            console.log(`success! password validated!`)
+    } else {
+        console.log(`Password could not be validated. Goodbye.`)
+        readline.close();
+    }
+  })
+  });
